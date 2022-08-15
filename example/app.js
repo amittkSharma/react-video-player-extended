6e50d008010cd9f836a992375bd7d1a915ce47ba
Sharma, Amit <asharma@agtinternational.com>
2022-08-15 17:44:22 +0200

    feat: Update the sample app, move header too take whole screen, align the props, settings and state

--- example/app.js
+++ example/app.js
@@ -149,87 +149,87 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <>
       <header className="main-header">
         <h1 className="app-name">React Video Player (Extended)</h1>
       </header>
-      <VideoPlayer
-        url={url}
-        controls={controls}
-        isPlaying={isPlaying}
-        volume={volume}
-        loop={false}
-        markers={markers}
-        height={'auto'}
-        width={'640px'}
-        timeStart={timeStart}
-        onPlay={handlePlay}
-        onPause={handlePause}
-        onVolume={handleVolume}
-        onProgress={handleProgress}
-        onDuration={handleDuration}
-        onMarkerClick={handleMarkerClick}
-        onMarkerAdded={handleMarkerAdd}
-        onVideoPlayingComplete={(props) => setIsPlaying(false)}
-        fps={fps}
-        selectedMarker={selectedMarker}
-        viewSettings={settings}
-      />
-      <div className="controls">
-        <p className="control-list">
-          Controls:
-          {controlsList.map((control) => {
-            return (
-              <label key={control.id.toString()} htmlFor={control.id}>
-                <input
-                  id={control.id}
-                  type="checkbox"
-                  checked={controls.includes(control.id)}
-                  onChange={handleControlToggle}
-                />{' '}
-                {control.title}
-              </label>
-            )
-          })}
-        </p>
-
-        <p className="control-list">
-          State:
-          <span style={{ height: 3 }} />
-          controls: {controls.join(', ')}
-          <span style={{ height: 3 }} />
-          isPlaying: {isPlaying.toString()}
-          <span style={{ height: 3 }} />
-          volume: {volume}
-          <span style={{ height: 3 }} />
-          timeStart: {timeStart}
-          <span style={{ height: 3 }} />
-          fps: {fps}
-          <span style={{ height: 3 }}></span>
-          selectedMarker:{' '}
-          {selectedMarker === undefined
-            ? 'No Marker is Selected'
-            : JSON.stringify(selectedMarker, null, 2)}
-        </p>
-
-        <p className="control-list">
-          Settings:
-          {settingsList.map((setting) => {
-            return (
-              <label key={setting.id.toString()} htmlFor={setting.id}>
-                <input
-                  id={setting.id}
-                  type="checkbox"
-                  checked={settings.includes(setting.id)}
-                  onChange={handleSettingToggle}
-                />{' '}
-                {setting.title}
-              </label>
-            )
-          })}
-        </p>
+      <div className="container">
+        <VideoPlayer
+          url={url}
+          controls={controls}
+          isPlaying={isPlaying}
+          volume={volume}
+          loop={false}
+          markers={markers}
+          height={'auto'}
+          width={'640px'}
+          timeStart={timeStart}
+          onPlay={handlePlay}
+          onPause={handlePause}
+          onVolume={handleVolume}
+          onProgress={handleProgress}
+          onDuration={handleDuration}
+          onMarkerClick={handleMarkerClick}
+          onMarkerAdded={handleMarkerAdd}
+          onVideoPlayingComplete={(props) => setIsPlaying(false)}
+          fps={fps}
+          selectedMarker={selectedMarker}
+          viewSettings={settings}
+        />
+        <div className="controls">
+          <em className="control-list">
+            Controls:
+            {controlsList.map((control) => {
+              return (
+                <label key={control.id.toString()} htmlFor={control.id}>
+                  <input
+                    id={control.id}
+                    type="checkbox"
+                    checked={controls.includes(control.id)}
+                    onChange={handleControlToggle}
+                  />
+                  {control.title}
+                </label>
+              )
+            })}
+          </em>
+          <em className="control-list">
+            Settings:
+            {settingsList.map((setting) => {
+              return (
+                <label key={setting.id.toString()} htmlFor={setting.id}>
+                  <input
+                    id={setting.id}
+                    type="checkbox"
+                    checked={settings.includes(setting.id)}
+                    onChange={handleSettingToggle}
+                  />{' '}
+                  {setting.title}
+                </label>
+              )
+            })}
+          </em>
+          <em className="control-list">
+            State:
+            <span style={{ height: 3 }} />
+            fps: {fps}
+            <span style={{ height: 3 }} />
+            timeStart: {timeStart}
+            <span style={{ height: 3 }} />
+            volume: {volume}
+            <span style={{ height: 3 }} />
+            isPlaying: {isPlaying.toString()}
+            <span style={{ height: 3 }} />
+            controls: {controls.join(', ')}
+            <span style={{ height: 3 }}></span>
+            selectedMarker:
+            {selectedMarker === undefined
+              ? 'No Marker is Selected'
+              : JSON.stringify(selectedMarker, null, 2)}
+          </em>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
